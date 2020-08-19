7bdbb9af332260056c80830adb823c3f3f9a4c52
Carlos Teller <carlos.z.teller@gmail.com>
2020-08-19 17:02:30 -0500

    Cleaned up grid/card layout

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Grid, Paper } from '@material-ui/core';
+import { Grid, Card, CardContent } from '@material-ui/core';
 
 export default function Pokemon({pokemon, search}) {
     return (
         <Grid container spacing={3}>
             {pokemon && pokemon.filter(p => p.name.toLowerCase().includes(search.toLowerCase())).map(p => (
                 <Grid item key={p.id} xs={12} sm={6} md={3}>
-                    <Paper>
-                        <img src={p.sprites.front_default} alt={p.name} />
-                        <p>No. {p.id}</p>
-                        <p>{p.name}</p>
-                        {p.types && p.types.map(t => (
-                            <p key={t.slot}>{t.type.name}</p>
-                        ))}
-                    </Paper>
+                    <Card>
+                        <CardContent>
+                            <img src={p.sprites.front_default} alt={p.name} />
+                            <p>No. {p.id}</p>
+                            <p>{p.name}</p>
+                            {p.types && p.types.map(t => (
+                                <p key={t.slot}>{t.type.name}</p>
+                            ))}
+                        </CardContent>
+                    </Card>
                 </Grid>
             ))}
         </Grid>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Paper, InputBase, IconButton } from '@material-ui/core';
+import { FormControl, Input, InputAdornment } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 export default function Search({change, term}) {
     return (
-        <Paper component="form">
-            <InputBase placeholder="Search Pokemon" value={term} onChange={change} />
-            <IconButton type="submit" aria-label="search">
-                <SearchIcon />
-            </IconButton>
-        </Paper>
+        <FormControl className="search" style={{ margin: "2em 0" }} fullWidth={true}>
+             <Input
+                id="search-pokemon"
+                placeholder="Search Pokemon"
+                value={term}
+                onChange={change}
+                style={{ padding: ".5em 1em" }}
+                endAdornment={ <InputAdornment position="end"><SearchIcon /></InputAdornment> }
+                inputProps={{ 'aria-label': 'search pokemon' }}   
+            />
+        </FormControl>
     );
 }
