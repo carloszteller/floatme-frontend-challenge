64d0e4324787927d08610942750f4bf37a937238
Carlos Teller <carlos.z.teller@gmail.com>
2020-08-21 11:53:21 -0500

    Updates

---
+++ precache-manifest.1c8e1f0f9a0ee8bb6f7876f54f68b3b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627eff790d7a280ada6aa4b95c6b19f9",
+    "url": "/floatme-frontend-challenge/index.html"
+  },
+  {
+    "revision": "2ad38129c01cc4f2818c",
+    "url": "/floatme-frontend-challenge/static/css/main.5594df3c.chunk.css"
+  },
+  {
+    "revision": "00f48e3a96c6c87baf90",
+    "url": "/floatme-frontend-challenge/static/js/2.4ef491f3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/floatme-frontend-challenge/static/js/2.4ef491f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad38129c01cc4f2818c",
+    "url": "/floatme-frontend-challenge/static/js/main.1729a388.chunk.js"
+  },
+  {
+    "revision": "735ed337862be1d674ba",
+    "url": "/floatme-frontend-challenge/static/js/runtime-main.c9e8bc92.js"
+  }
+]);
